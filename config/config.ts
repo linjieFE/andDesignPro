6a912618d7f767e1c038c3924d7471c55fda0eec
linjie.zheng <linjie.zheng@flexiv.com>
2022-02-25 19:21:05 +0800

    config/config.ts中配置项目和router.ts

--- config/config.ts
+++ config/config.ts
@@ -2,5 +2,11 @@ import { defineConfig } from 'umi';
 import routes from './routes';
 
 export default defineConfig({
+  nodeModulesTransform:{
+    type: 'none'
+  },
+  layout: {},
   routes: routes,
+  fastRefresh: {},
+  mfsu:{}
 });
